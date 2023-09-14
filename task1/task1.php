af48e360d9892980389879f5e14f02de5d8ed256
Ahmed234606 <ahmedmohamedhamdyali@gmail.com>
2023-09-14 03:04:26 +0100

    task finshed

--- task1/task1.php
+++ task1/task1.php
@@ -1,7 +1,8 @@
 <?php
-
-
-
+$task1_1='about Network Layers Explained: OSI & TCP/IP Models';
+$task1_2='difference between echo and print in php';
+$task1_3='print with concatenate';
+echo('task1' . $task1_1 .'<br/>' . $task1_2 .'<br/>' . $task1_3);
 
 ?>
 
@@ -11,9 +12,147 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>task1</title>
+   
 </head>
 <body>
-    
+<iframe class='iframe' src="https://www.connect4techs.com/%D8%B4%D8%B1%D8%AD-osi-layers/"></iframe>
+<header class="header">
+        <section class="head">
+        <h1 >
+            OSI-7-layers
+        </h1>
+    </section>
+    <section class="head">
+        <h1 >
+            Tcp-layers
+        </h1>
+        
+    </section >
+        </header>
+
+        <section class="layer">
+        <section class="OSI-7-layers">
+        <section class="applicatoin_layer">
+            <div class="app">
+               7 
+            </div>
+            <div class="app">
+            Applicatoin  layer
+            </div>
+            <div class="app">
+                human-computer intraction layer where applicatins can access the network service examples( Wep applicatin)
+
+            </div>
+        </section>
+        <section class="Presentatoin_layer">
+            <div class="pre">
+               6 
+            </div>
+            <div class="pre">
+                Presentatoin layer
+            </div>
+            <div class="pre">
+                Ensures that data is in usable format and is where data encryption occurs examples( HTTP )
+
+            </div>
+        </section>
+        <section class="Session_layer">
+            <div class="ses">
+               5
+            </div>
+            <div class="ses">
+                Session layer
+            </div>
+            <div class="ses">
+                maintains connections and is responsible for controlling ports and sessions
+                examples( port 80 )
+
+            </div>
+        </section>
+        <section class="Transport_layer">
+            <div class="tra">
+               4
+            </div>
+            <div class="tra">
+                Transport layer
+            </div>
+            <div class="tra">
+                transmits data using transmission  protogols inculding TCP and UDP
+
+            </div>
+        </section>
+        <section class="Network_layer">
+            <div class="net">
+                     3
+            </div>
+            <div class="net">
+                Network layer
+            </div>
+            <div class="net">
+                Decides which physical path the data will take
+            </div>
+        </section>
+        <section class="Data_Link_layer">
+            <div class="dat">
+             2
+            </div>
+            <div class="dat">
+                Data Link  layer
+            </div>
+            <div class="dat">
+                 Define the format of data on the network
+            </div>
+        </section>
+        <section class="Physical_layer">
+            <div class="phy">
+              1
+            </div>
+            <div class="phy">
+                Physical layer
+            </div>
+            <div class="phy">
+                transmits raw bit stream over the physical modium
+            </div>
+        </section>
+    </section>
+
+    <section class="OSI-7-layers">
+        <section class="Applicatoin">
+            
+            <div class="appl">
+            Applicatoin  layer
+            </div>
+            
+        </section>
+        <section class="Transport">
+           
+            <div class="tran">
+                Transport layer
+            </div>
+            
+        </section>
+        <section class="Intrnet">
+            
+            <div class="int">
+                Intrnet layer
+            </div>
+           
+        </section>
+        <section class="Network">
+            
+            <div class="netw">
+                Network Acsses layer
+            </div>
+            
+        </section>
+    </section>
+   </section>
+   <section class="mainimg" >
+
+<img src="echoandprint.png" alt="" srcset="">
+
+</section>
 </body>
 </html>
